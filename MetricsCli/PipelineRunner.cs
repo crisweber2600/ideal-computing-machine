70e7278c6fdbf5a125cc760778be392b724983d4
Cris Weber <50303604+crisweber2600@users.noreply.github.com>
2025-06-19 17:39:41 -0500

    Enhance CLI env defaults and cancellation

--- MetricsCli/PipelineRunner.cs
+++ MetricsCli/PipelineRunner.cs
@@ -9,18 +9,19 @@ public static class PipelineRunner
         IDriveScanner googleScanner,
         IDriveScanner microsoftScanner,
         Stream output,
-        ILoggerFactory loggerFactory)
+        ILoggerFactory loggerFactory,
+        CancellationToken cancellationToken = default)
     {
-        var gScanner = new DirectoryScanner(googleScanner, loggerFactory.CreateLogger<DirectoryScanner>());
-        var mScanner = new DirectoryScanner(microsoftScanner, loggerFactory.CreateLogger<DirectoryScanner>());
-        var googleCounts = await gScanner.ScanAsync(options.GoogleRoot, options.GoogleRoot);
-        var microsoftCounts = await mScanner.ScanAsync(options.MsRoot, options.MsRoot);
+        var gScanner = new DirectoryScanner(googleScanner, loggerFactory.CreateLogger<DirectoryScanner>(), options.MaxDop);
+        var mScanner = new DirectoryScanner(microsoftScanner, loggerFactory.CreateLogger<DirectoryScanner>(), options.MaxDop);
+        var googleCounts = await gScanner.ScanAsync(options.GoogleRoot, options.GoogleRoot, cancellationToken);
+        var microsoftCounts = await mScanner.ScanAsync(options.MsRoot, options.MsRoot, cancellationToken);
 
         var comparer = new DirectoryCountsComparer();
         var differences = comparer.Compare((IReadOnlyDictionary<string, DirectoryCounts>)googleCounts,
             (IReadOnlyDictionary<string, DirectoryCounts>)microsoftCounts);
         var exporter = new CsvExporter();
-        await exporter.ExportAsync(differences, output);
+        await exporter.ExportAsync(differences, output, cancellationToken);
     }
 }
 
--- MetricsPipeline.Core.Tests/Features/CommandLine.feature
+++ MetricsPipeline.Core.Tests/Features/CommandLine.feature
@@ -8,6 +8,24 @@ Feature: Command Line
     When I parse the arguments "--ms-root m --google-root g"
     Then the parsed options should contain "cred.json"
 
+  Scenario: root paths from environment
+    Given MS_ROOT is set to "m"
+    And GOOGLE_ROOT is set to "g"
+    And OUTPUT_CSV is set to "out.csv"
+    When I parse the arguments ""
+    Then the options MsRoot should be "m"
+    And the options GoogleRoot should be "g"
+    And the output path should be "out.csv"
+
+  Scenario: max dop from environment
+    Given MS_ROOT is set to "m"
+    And GOOGLE_ROOT is set to "g"
+    And environment variable GOOGLE_AUTH is set to "cred.json"
+    And OUTPUT_CSV is set to "out.csv"
+    And MAX_DOP is set to "8"
+    When I parse the arguments ""
+    Then the max dop should be 8
+
   Scenario: exporting CSV results
     When I run the CLI pipeline
     Then the output should contain a CSV header
